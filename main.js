0d28e37df6278f36ffcdb9189fd3992af68700b9
SalimovMirfayz <148865250+SalimovMirfayz@users.noreply.github.com>
2023-11-16 11:27:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// let son = 10;
+
+// while (son >= 1) {
+//     console.log(son);
+//     son = son - 1;
+// }
+
+// let son = 1;
+// while(son  <= 100 ){
+// console.log(son)
+// son = son + 2
+// }
+
+let a = "#" ;
+while (a <= "##########" ){
+  console.log(a)
+  a +="#"
+}
